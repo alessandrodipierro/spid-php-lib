00bfc0791cc74566b7aaeb8252325ffc1d19f2ac
andreadegiovine <andreadegiovine90@gmail.com>
2021-04-27 17:15:50 +0200

    fix: check if spid level is valid and level 1;

--- src/Spid/Saml/In/Response.php
+++ src/Spid/Saml/In/Response.php
@@ -195,15 +195,17 @@ class Response implements ResponseInterface
             // Status code != success
             return false;
         }
-
-        $session = $this->spidSession($xml);
         
         //check Spid level, accept only response with level 1
-        if(!isset($session->level) || $session->level != 1) {
-            throw new \Exception("Invalid Spid response level, expected 1 but received " . $session->level);
+        if(!isset($xml->getElementsByTagName('AuthnContextClassRef')->item(0)->nodeValue) ||
+        !strpos($xml->getElementsByTagName('AuthnContextClassRef')->item(0)->nodeValue, 'urn:oasis:names:tc:SAML:2.0:ac:classes:SpidL') ||
+        substr($xml->getElementsByTagName('AuthnContextClassRef')->item(0)->nodeValue, -1) != 1) {
+            throw new \Exception("Invalid AuthnContextClassRef, expected 'urn:oasis:names:tc:SAML:2.0:ac:classes:SpidL1' but received " . 
+            $xml->getElementsByTagName('AuthnContextClassRef')->item(0)->nodeValue);
         }
 
         // Response OK
+        $session = $this->spidSession($xml);
         $_SESSION['spidSession'] = (array)$session;
         unset($_SESSION['RequestID']);
         unset($_SESSION['idpName']);
