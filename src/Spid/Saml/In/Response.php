af5132f7a67c1fa0dcaa40f9e510b7332da17f78
andreadegiovine <andreadegiovine90@gmail.com>
2021-04-28 14:55:03 +0200

    fix: issueinstant past check;

--- src/Spid/Saml/In/Response.php
+++ src/Spid/Saml/In/Response.php
@@ -82,8 +82,7 @@ class Response implements ResponseInterface
                 throw new \Exception("IssueInstant attribute on Assertion is in the future");
             } elseif (strtotime($xml->getElementsByTagName('Assertion')->item(0)->getAttribute('IssueInstant')) <=
                 strtotime('now') + $accepted_clock_skew_seconds) {
-                throw new \Exception("IssueInstant: " . strtotime($xml->getElementsByTagName('Assertion')->item(0)->getAttribute('IssueInstant')) . "\n" .
-            "Now: " . strtotime('now') + $accepted_clock_skew_seconds);
+                throw new \Exception("Now: " . strtotime('now') + $accepted_clock_skew_seconds);
                 // throw new \Exception("IssueInstant attribute on Assertion is in the past");
             }
 
